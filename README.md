4416f8ad30ab002d5914def6b2221fdda76e0cd9
gudtjr3437 <gudtjr3437@naver.com>
2021-01-14 19:53:30 +0900

    #4.1 User Model

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-#Nuber Eats
-
+# Nuber Eats
 
 The Backend of Nuber Eats Clone
+
+## User Entity:
+
+- id
+- createdAt
+- UpdatedAt
+
+- email
+- password
+- role(client|owner|delivery)
+
+## User CRUD:
+
+- Create Account       
+- Log In
+- See Profile
+- Edit Profile
+- verify Email
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import {Restaurant} from './restaurants/entities/restaurant.entity';
+import { UsersModule } from './users/users.module';
+import { CommonModule } from './common/common.module';
+import { User } from './users/entities/user.entity';
 
 console.log(Joi);
 
@@ -26,8 +29,9 @@ console.log(Joi);
       database: "db.sqlite" ,
       synchronize: process.env.NODE_ENV !=='prod',
       logging: process.env.NODE_ENV !== 'prod',
-      entities: [Restaurant],
-    }), RestaurantsModule],
+      entities: [Restaurant,User],
+      
+    }), RestaurantsModule, UsersModule, CommonModule],
   controllers: [],
   providers: [],
 })
